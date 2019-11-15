59ff8b69998890e119e5a1f237b8eb3e2ed02853
Lance Sell <lms223@lehigh.edu>
2019-11-15 00:22:35 -0500

    assignment 5
    assignment 05

---
+++ Assignment05.R
@@ -0,0 +1,13 @@
+ptm <- proc.time()
+DF <- read.csv("UNRATE.csv")
+CSV_READ_TIME <- (proc.time() - ptm)
+CSV_READ_TIME
+
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+
+ptm <- proc.time()
+DF <- fread("UNRATE.csv", header="auto", 
+            data.table=FALSE)
+FREAD_READ_TIME <- (proc.time() - ptm)
+FREAD_READ_TIME
